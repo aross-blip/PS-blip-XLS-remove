0c209046b6b4ac31d9dcf72eb6430b194535f2c0
aross-blip <69678715+aross-blip@users.noreply.github.com>
2021-06-15 11:25:33 +0300

    Update delXLSX.ps1

--- delXLSX.ps1
+++ delXLSX.ps1
@@ -6,23 +6,24 @@ $path = Read-Host "Please input full path to .xlsx file"
 
 $xlsxObj = new-object System.Collections.ArrayList
 $dirName = [System.IO.Path]::GetDirectoryName($path)
+#Import .xlsx file using module psexcel
 foreach ($line in (Import-XLSX -Path $path -RowStart 1)){
     $xlsxObj.add($line) | out-null 
 }
+#Clear errors before the sript runs, to avoid writing previous errors to log file 
 $Error.Clear()
-
+    #Iterate through .xlsx file and remove path if it exists
     for ($x=0;$x -lt $xlsxObj.Count;$x++) { 
         if ([System.IO.File]::Exists("$($xlsxObj.Path[$x])"+"$($xlsxObj.Filename[$x])")){
             $counter = 1
             rm ("$($xlsxObj.Path[$x])"+"$($xlsxObj.Filename[$x])")
-            #"$($x+1) File ($($xlsxObj.Path[$x])"+"$($xlsxObj.Filename[$x])) succesfully deleted" | Out-File $dirName\del-info.log -Append
-             #[string[]]$successCount = $($xlsxObj.Path[$x])"+"$($xlsxObj.Filename[$x])
             $counter +=1
             
         } else {
+            #Append to log file which line in the excel sheet could not be deleted
             "Excel line $($x+1) [ERROR]: $($xlsxObj.Path[$x])"+"$($xlsxObj.Filename[$x]) not found. Could not be deleted"| Out-File $dirName\del-info.log -Append
         }
     
     } 
-    #"Successfully deleted $counter files" | Out-File $dirName\del-info.log -Append
+    #Write to del-info.log
     @("Successfully deleted $counter files")+ (Get-Content $dirName\del-info.log) | Set-Content $dirName\del-info.log
